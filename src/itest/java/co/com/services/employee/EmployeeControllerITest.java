6c40b987d0cb8e6599077d56492f1ffcd120cfa6
Wilman Ortiz <willman.ortiz@gmail.com>
2022-01-08 16:19:23 -0500

    Implementa pruebas de integracion

--- src/itest/java/co/com/services/employee/EmployeeControllerITest.java
+++ src/itest/java/co/com/services/employee/EmployeeControllerITest.java
@@ -1,38 +1,76 @@
 package co.com.services.employee;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Testcontainers
+@AutoConfigureMockMvc
 @ActiveProfiles("itest")
 @ExtendWith(SpringExtension.class)
-@AutoConfigureWebTestClient(timeout = "36000")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
-//@Sql({ "file:src/itest/resources/database/employees.sql" })
+@DisplayName("TEST :: Bateria de pruebas de integracion para la informacion de los empleados registrados")
 public class EmployeeControllerITest {
 
     @Autowired
-    private WebTestClient client;
+    private MockMvc client;
 
     @Test
-    void itShouldReturnPositionAndMessage() throws Exception {
-        this.client
-                // Create a GET request to test an endpoint
-                .get().uri("/employees/api/v1")
-                .accept(MediaType.TEXT_EVENT_STREAM)
-                .exchange()
-                // and use the dedicated DSL to test assertions against the response
-                .expectStatus().isOk();
+    @DisplayName("TEST :: Valida consulta de todos los empleados :: paginando los resultados")
+    void itShouldReturnEmployeesInformation() throws Exception {
+
+        this.client.perform( MockMvcRequestBuilders
+                        .get("/employees/api/v1/?page=0&size=5")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.[0].code").isNumber());
+    }
+
+    @Test
+    @DisplayName("TEST :: Valida consulta de un empleado por 'code'")
+    void itShouldReturnEmployeesInformationDetailById() throws Exception {
+        this.client.perform( MockMvcRequestBuilders
+                        .get("/employees/api/v1/10001")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").isNumber());
+    }
+
+    @Test
+    @DisplayName("TEST :: Valida 'BAD_REQUEST' al consultar informacion del empleado")
+    void itShouldReturnEmployeesInformationDetailCodeValueException() throws Exception {
+        this.client.perform( MockMvcRequestBuilders
+                        .get("/employees/api/v1/A")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(HttpStatus.BAD_REQUEST.name()));
+    }
+
+    @Test
+    @DisplayName("TEST :: Valida 'BAD_REQUEST' al consultar informacion del empleado")
+    void itShouldReturnEmployeesInformationDetailByIdNotFound() throws Exception {
+        this.client.perform( MockMvcRequestBuilders
+                        .get("/employees/api/v1/1234567")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(HttpStatus.NOT_FOUND.name()));
     }
 
 }
