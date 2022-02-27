244519e9f89d61a6007c689523324881288fcc95
Wilman Ortiz <willman.ortiz@gmail.com>
2022-02-26 21:24:07 -0500

    Agrega ajuste para ejecucion mysql con testcontainer en jenkins

--- src/itest/java/co/com/services/employee/MySQLTest.java
+++ src/itest/java/co/com/services/employee/MySQLTest.java
@@ -9,6 +9,8 @@ public class MySQLTest {
     public void testSelect() {
         MySQLContainer mysql = new MySQLContainer("mysql:8.0.27");
 
+        mysql.withCommand("mysqld --default-authentication-plugin=mysql_native_password");
+
         mysql.start();
         mysql.stop();
 
